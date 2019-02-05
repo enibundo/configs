0bdb6264bbbd1fb4b9f3037d264e5c78d37ba9cb
Eni BUNDO <eni.bundo@gmail.com>
2019-02-05 15:33:53 +0100

    changes

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # copy rc.lua
+cp rc.lua ~/.config/awesome
 
 # copy .bashrc
-
+cp .bashrc ~/.bashrc
 
 # install :
 # - caja (file manager gui)
@@ -13,7 +14,7 @@
 sudo apt-get install caja emacs doublecmd-gtk xsettingsd acpi
 
 # copy emacs .rc
-
+cp .emacs ~/
 
 # install termite
 wget https://raw.githubusercontent.com/Corwind/termite-install/master/termite-install.sh
